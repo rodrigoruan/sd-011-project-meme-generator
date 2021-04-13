c3ee9915219fa736c2b7b64b5e54e15b9461bbff
Tales Coelho <tales-coelho@live.com>
2021-04-13 16:19:20 -0300

    Requisito 3

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 const textInput = document.getElementById('text-input');
 const memeText = document.getElementById('meme-text');
+const memeInsert = document.getElementById('meme-insert');
+const memeImage = document.getElementById('meme-image');
+
 
 textInput.addEventListener('keyup', () => {
   memeText.innerText = textInput.value;
-})
+});
+
+memeInsert.addEventListener('click', () => {
+  const imageMeme = document.createElement('img');
+  imageMeme.src = 'img1.jpg';
+  imageMeme.classList.add('memeSize');
+  memeImage.appendChild(imageMeme);
+});
