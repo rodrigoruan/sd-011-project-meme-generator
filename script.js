4b29c4f542f0841950136feed089e10bb8defc3e
Jean Esteves <jean.esteves2001@gmail.com>
2021-04-13 17:36:41 -0300

    erro do lint resolvido ao extremo

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const getInputText = document.querySelector('#text-input');
 const output = document.getElementById('meme-image');
 const getP = document.querySelector('#meme-text');
+const getMemeInsert = document.querySelector('#meme-insert');
 const getContainer = document.querySelector('#meme-image-container');
 const getFire = document.querySelector('#fire');
 const getWater = document.querySelector('#water');
@@ -9,6 +10,19 @@ const getMeme1 = document.querySelector('#meme-1');
 const getMeme2 = document.querySelector('#meme-2');
 const getMeme3 = document.querySelector('#meme-3');
 const getMeme4 = document.querySelector('#meme-4');
+
+getMemeInsert.onchange = function loadFile(event) {
+  const reader = new FileReader();
+  reader.onload = () => {
+    output.src = reader.result;
+  };
+  reader.readAsDataURL(event.target.files[0]);
+};
+/* resolver o exercício = https://stackoverflow.com/questions/4459379/preview-an-image-before-it-is-uploaded */
+/* FileReader = https://developer.mozilla.org/pt-BR/docs/Web/API/FileReader */
+/* operator new = https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Operators/new */
+/* readAsDataURL = https://developer.mozilla.org/pt-BR/docs/Web/API/FileReader/readAsDataURL */
+/* onchange in js = https://qastack.com.br/programming/5024056/how-to-pass-parameters-on-onchange-of-html-select */
 
 function textInputKeyUp() {
   getInputText.addEventListener('keyup', () => {
@@ -22,18 +36,6 @@ function border(style) {
   getContainer.style.border = style;
 }
 border('1px solid black');
-
-loadFile = (event) => {
-  const reader = new FileReader();
-  reader.onload = () => {
-    output.src = reader.result;
-  };
-  reader.readAsDataURL(event.target.files[0]);
-};
-/* resolver o exercício = https://stackoverflow.com/questions/4459379/preview-an-image-before-it-is-uploaded */
-/* FileReader = https://developer.mozilla.org/pt-BR/docs/Web/API/FileReader */
-/* operator new = https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Operators/new */
-/* readAsDataURL = https://developer.mozilla.org/pt-BR/docs/Web/API/FileReader/readAsDataURL */
 
 function addBorderButtons() {
   getFire.addEventListener('click', () => {
