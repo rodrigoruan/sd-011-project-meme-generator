f55048a848b3dd65e6e3dac35992696e517cdcf1
Daniel <danielfdoficial@gmail.com>
2021-04-13 16:50:23 -0300

    conserta tudo

--- script.js
+++ script.js
@@ -13,10 +13,7 @@ clickButton();
 
 function inputText() {
   input.addEventListener('keyup', function(event) {
-    if (event.keyCode === 13) {
-      event.preventDefault();
       document.getElementById('benjaminButton').click();
-    }
   });
 };
 inputText();
