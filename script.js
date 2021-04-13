094e0d28f4605137598adaf5496214f5a65a2cac
Jose Antonio Moura Neto <netomoura22@gmail.com>
2021-04-13 16:43:22 -0300

    cumprimento do requisito 1 /3

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ const caixaDeTexto = document.querySelector('#text-imput');
 const textoMeme = document.querySelector('#meme-text');
 
 caixaDeTexto.addEventListener('keyup', () => {
-  textoMeme.innerHTML = caixaDeTexto.value;
+  textoMeme.innerHTML = caixaDeTexto.target.value;
 });
