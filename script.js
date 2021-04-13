2420fa38d0e8b1af1bf6a9db7b08e4c84165edc0
Diego Ribeiro <dihrm2001@gmail.com>
2021-04-13 17:14:50 -0300

    Requisito 6

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const text = document.querySelector('#text-input');
 const imageInput = document.querySelector('#meme-insert');
+const container = document.querySelector('#meme-image-container');
+const btnsBorder = document.querySelector('#border-btns');
 
 function showText() {
   const newP = document.querySelector('#meme-text');
@@ -10,7 +12,15 @@ text.addEventListener('keyup', showText);
 
 function showImage(event) {
   const newImg = document.querySelector('#meme-image');
+  container.className = 'default-border';
   newImg.src = URL.createObjectURL(event.target.files[0]);
 }
 
 imageInput.addEventListener('change', showImage);
+
+function addBorder(event) {
+  const btn = event.target;
+  container.className = btn.id;
+}
+
+btnsBorder.addEventListener('click', addBorder);
