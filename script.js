d4eef48ef6db1834f6be4961f982f37e830fd1d5
Matheus_Alexandre <alexandremhm@gmail.com>
2021-04-13 20:40:15 -0300

    refatora requisitos 1-3

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const photoDiv = document.querySelector('#meme-image-container');
 const imageContainer = document.querySelector('#meme-image')
 
 function writeMeme() {
-  inputMeme.addEventListener('keyup', (event) => {    
+  inputMeme.addEventListener('keyup', () => {    
       const divText = document.querySelector('#meme-text');
       divText.innerText = inputMeme.value;
   });
@@ -12,11 +12,19 @@ function writeMeme() {
 
 writeMeme();
 
-memeInsert.addEventListener('change', (upPhoto) => {
+function showImage(event2) {
   const imageMeme = document.createElement('img');
-  const memeURL = upPhoto.target.value;
-  imageMeme.setAttribute('src', memeURL)
-  imageMeme.classList.add('meme-size')
-  imageContainer.appendChild(imageMeme);
-})
+  imageMeme.classList.add('meme-size');
+  imageMeme.src = URL.createObjectURL(event2.target.files[0]);
+  imageContainer.appendChild(imageMeme);  
+}
+
+memeInsert.addEventListener('change', showImage);
+
+
+  
+
+
+
+
 
