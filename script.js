a6484e7ca9fd2efad2eed739afb97c4a9976c6b9
PH Assis <pieit@live.com>
2021-04-13 19:23:44 -0300

    chl 7

--- script.js
+++ script.js
@@ -3,12 +3,15 @@ const memeContainer = document.getElementById('meme-image-container');
 const memeText = document.getElementById('meme-text');
 const inputImage = document.getElementById('meme-insert');
 const outputImage = document.getElementById('meme-image');
+const locateFileButton = document.getElementById('locateFile');
+const memeTemplate = document.getElementsByClassName('meme-templates');
+const memePNG = document.getElementById('meme-png');
 
 function textInBox() {
   memeText.innerText = inputBox.value;
 }
 
-inputBox.addEventListener('keypress', textInBox);
+inputBox.addEventListener('keyup', textInBox);
 
 function imageInBox(event) {
   const x = document.createElement('img');
@@ -17,3 +20,18 @@ function imageInBox(event) {
 }
 
 inputImage.addEventListener('change', imageInBox);
+
+locateFileButton.addEventListener('click', () => {
+  inputImage.click();
+});
+
+function useTemplate() {
+  for (let i = 0; i < memeTemplate.length; i += 1) {
+    memeTemplate[i].addEventListener('click', () => {
+      memePNG.removeAttribute('hidden');
+      const x = memePNG;
+      x.src = memeTemplate[i].src;
+    });
+  }
+}
+useTemplate();
