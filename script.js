83a37531635de0f8259b7362ca2c2dce9c8be776
Marco Galindo <marco1408@gmail.com>
2021-04-13 18:59:49 -0300

    requisito 2 finalizado

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
+// 1 - Crie uma caixa de texto com a qual quem usa pode interagir para inserir texto em cima da imagem escolhida.
+
 const textInput = document.querySelector('#text-input');
 const memeText = document.querySelector('#meme-text');
 
 textInput.onkeyup = function createMemeText() {
   memeText.innerText = textInput.value;
 };
+
+// 2 - O site deve permitir que quem usa faça upload de uma imagem de seu computador.
+
+const memeInsert = document.querySelector('#meme-insert');
+const memeImageContainer = document.querySelector('#meme-image-container');
+
+function uploadImage(event) {
+  const memeImage = document.createElement('img');
+  memeImage.innerHTML = '';
+  memeImage.id = 'meme-image';
+  memeImage.src = URL.createObjectURL(event.target.files[0]);
+  memeImageContainer.appendChild(memeImage);
+}
+
+memeInsert.addEventListener('input', uploadImage);
