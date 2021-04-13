8e8f4938dc589d462f2d36fdc33d6d700e0e86fd
Jean Esteves <jean.esteves2001@gmail.com>
2021-04-13 15:07:59 -0300

    resolvendo alguns erros do lint

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const getInputText = document.querySelector('#text-input');
 const getP = document.querySelector('#meme-text');
-const getInputImage = document.querySelector('#meme-insert')
 
 function textInputKeyUp() {
   getInputText.addEventListener('keyup', () => {
@@ -10,13 +9,15 @@ function textInputKeyUp() {
 }
 textInputKeyUp();
 
-var loadFile = function (event) {
-  var reader = new FileReader();
-  reader.onload = function () {
-  var output = document.getElementById('meme-image');
-  output.src = reader.result;
+loadFile = (event) => {
+  const reader = new FileReader();
+  reader.onload = () => {
+    const output = document.getElementById('meme-image');
+    output.src = reader.result;
   };
   reader.readAsDataURL(event.target.files[0]);
 };
+/* resolver o exercício = https://stackoverflow.com/questions/4459379/preview-an-image-before-it-is-uploaded */
 /* FileReader = https://developer.mozilla.org/pt-BR/docs/Web/API/FileReader */
-/* https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Operators/new */
+/* operator new = https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Operators/new */
+/* readAsDataURL = https://developer.mozilla.org/pt-BR/docs/Web/API/FileReader/readAsDataURL */
