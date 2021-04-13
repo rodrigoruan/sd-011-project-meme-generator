4d0ae2e42510006b988a2ab25dcc995ff4340aa8
Alberto Candido <albertocandido98@gmail.com>
2021-04-13 20:30:30 -0300

    Requisito 6 - acrescenta tipos de borda para imagem

--- script.js
+++ script.js
@@ -1,19 +1,40 @@
 const inputMeme = document.querySelector('#text-input');
 const inputImg = document.querySelector('#meme-insert');
+const container = document.querySelector('#meme-image-container');
 
-function writeMeme() {
-  inputMeme.addEventListener('keyup', () => {
-    const textArea = document.querySelector('#meme-text');
-    textArea.innerText = inputMeme.value;
-  });
-}
+inputMeme.addEventListener('keyup', () => {
+  const textArea = document.querySelector('#meme-text');
+  textArea.innerText = inputMeme.value;
+});
 
-writeMeme();
-
-inputImg.addEventListener('change', (e) => {
+// 2
+inputImg.addEventListener('change', (e1) => {
   const imgArea = document.querySelector('#meme-image'); // pai
+  imgArea.innerHTML = '';
   const memeImg = document.createElement('img'); // cria tag
-  memeImg.setAttribute('src', e.target.value);
+  memeImg.setAttribute('src', URL.createObjectURL(e1.target.files[0]));
   memeImg.classList.add('img');
   imgArea.appendChild(memeImg);
 });
+
+// 6
+
+function apBorder(e) {
+  if (e.target.id === 'fire') {
+    container.style.border = '3px dashed red';
+    console.log('teste');
+  } else if (e.target.id === 'water') {
+    container.style.border = 'double 5px blue';
+    console.log('teste2');
+  } else if (e.target.id === 'earth') {
+    container.style.border = 'groove 6px green';
+    console.log('teste3');
+  }
+}
+
+function changeBorder() {
+  const buttons = document.getElementById('buttons');
+  buttons.addEventListener('click', apBorder);
+}
+
+changeBorder();
