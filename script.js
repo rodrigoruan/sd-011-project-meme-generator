c085976442362596ccc2da855d7a7f6c4acf0043
Gustavo Lemes <gustavolemes997@gmail.com>
2021-04-13 16:38:22 -0300

    Requisito 2

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 const inputText = document.querySelector('#text-input');
+const inputFile = document.querySelector('#meme-insert');
 const memeContainerText = document.querySelector('#meme-text');
+const memeContainerImg = document.querySelector('#meme-image');
 
 inputText.addEventListener('keyup', () => {
   memeContainerText.innerText = inputText.value;
+});
+
+inputFile.addEventListener('input', () => {
+  const urlImage = URL.createObjectURL(inputFile.files[0]);
+  memeContainerImg.setAttribute('src', urlImage);
 });
