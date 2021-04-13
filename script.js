8d53228106bfef37e17d57f0c10f01e90c78eec1
Tales Coelho <tales-coelho@live.com>
2021-04-13 15:30:21 -0300

    Trocando keypresss para keyup

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 const textInput = document.getElementById('text-input');
 const memeText = document.getElementById('meme-text');
 
-textInput.addEventListener('keydown', (event) => {
-  if (event.code === 'Enter' && textInput.value) {
-    memeText.innerText = textInput.value;
-  }
+textInput.addEventListener('keyup', () => {
+  memeText.innerText = textInput.value;
 })
