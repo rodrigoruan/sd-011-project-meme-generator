1400f4b98e5cba126ee5ee032a11f594c6fb5ff6
Jean Esteves <jean.esteves2001@gmail.com>
2021-04-13 15:39:50 -0300

    requisito 6 feito

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const getInputText = document.querySelector('#text-input');
 const getP = document.querySelector('#meme-text');
+const getContainer = document.querySelector('#meme-image-container');
+const getFire = document.querySelector('#fire');
+const getWater = document.querySelector('#water');
+const getEarth = document.querySelector('#earth');
 
 function textInputKeyUp() {
   getInputText.addEventListener('keyup', () => {
@@ -8,6 +12,11 @@ function textInputKeyUp() {
   });
 }
 textInputKeyUp();
+
+function border(style) {
+  getContainer.style.border = style;
+}
+border('1px solid black');
 
 loadFile = (event) => {
   const reader = new FileReader();
@@ -21,3 +30,16 @@ loadFile = (event) => {
 /* FileReader = https://developer.mozilla.org/pt-BR/docs/Web/API/FileReader */
 /* operator new = https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Operators/new */
 /* readAsDataURL = https://developer.mozilla.org/pt-BR/docs/Web/API/FileReader/readAsDataURL */
+
+function addBorderButtons() {
+  getFire.addEventListener('click', () => {
+    border('3px dashed red');
+  });
+  getWater.addEventListener('click', () => {
+    border('5px double blue');
+  });
+  getEarth.addEventListener('click', () => {
+    border('6px groove green');
+  });
+}
+addBorderButtons();
