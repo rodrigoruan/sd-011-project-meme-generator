6ed8e0f87f09262759226980d333d01b036aae2c
Samuel Melo <samuel.silva.melo@hotmail.com>
2021-04-13 18:44:09 -0300

    Altera função inserir imagem e adiciona função inserir texto

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
-const chooseFile = document.querySelector('#meme-image');
-const memeImageContainer = document.querySelector('#meme-image-container');
+const chooseFile = document.querySelector('#meme-insert');
+// const memeImageContainer = document.querySelector('#meme-image-container');
+const textInput = document.querySelector('#text-input');
+const memeText = document.querySelector('#meme-text');
 
 function addImage() {
   const uploadedImage = chooseFile.files;
-  const createImage = document.createElement('img');
-  createImage.id = 'image';
+  const createImage = document.querySelector('#meme-image');
   createImage.src = URL.createObjectURL(uploadedImage[0]);
-
-  memeImageContainer.removeChild(memeImageContainer.lastElementChild);
-
-  memeImageContainer.appendChild(createImage);
 }
 
 chooseFile.addEventListener('change', addImage);
+
+//
+
+function addText() {
+  const textValue = textInput.value;
+  memeText.innerText = textValue;
+}
+
+textInput.addEventListener('keyup', addText);
