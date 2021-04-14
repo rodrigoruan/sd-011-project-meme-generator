02cbabdd31d97e0aeadecf0e853e8c6d5adc6464
Guilherme Pereira de Oliveira <guilhermepdo1508@gmail.com>
2021-04-14 12:19:46 -0300

    Requisito 1

---
+++ script.js
@@ -0,0 +1,8 @@
+const textInput = document.querySelector('#text-input');
+const memeText = document.querySelector('#meme-text');
+
+[textInput].forEach((item) => {
+  item.addEventListener('keyup', () => {
+    memeText.innerText = textInput.nodeValue;
+  });
+});
