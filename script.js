44d5ac7c46d46a189f6ae45464e45ab9465bf221
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-04-13 23:47:38 -0300

    adiciona requisito 02

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const inputText = document.getElementById('text-input');
+const inputFile = document.getElementById('meme-insert');
+const imgMeme = document.getElementById('meme-image');
 const spanMemeText = document.getElementById('meme-text');
 
 function setMemeText() {
@@ -6,8 +8,25 @@ function setMemeText() {
   spanMemeText.innerText = text;
 }
 
+/*
+  File Reader method taken from StackOverflow
+  Source: https://stackoverflow.com/questions/5802580/html-input-type-file-get-the-image-before-submitting-the-form
+*/
+function setMemeImage() {
+  const reader = new FileReader();
+  const file = inputFile.files[0];
+
+  reader.onloadend = () => {
+    imgMeme.src = reader.result;
+  };
+
+  if (file) reader.readAsDataURL(file);
+  else imgMeme.src = '';
+}
+
 function onLoad() {
   inputText.addEventListener('input', setMemeText);
+  inputFile.addEventListener('change', setMemeImage);
 }
 
 window.onload = onLoad;
