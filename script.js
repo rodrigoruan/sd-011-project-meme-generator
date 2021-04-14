e38d00c2b3e9158bc142478fe0937072834882bd
Matheus Gaspar <msouzagaspar@gmail.com>
2021-04-14 10:58:05 -0300

    requisito 6 e 7: adiciona imagens pré-prontas e botoes para estilizar borda

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ const textInputElement = document.getElementById('text-input');
 const memeTextElement = document.getElementById('meme-text');
 const memeInsertElement = document.getElementById('meme-insert');
 const imageElement = document.getElementById('meme-image');
+const memeImgContainerElement = document.getElementById('meme-image-container');
+const fireButton = document.getElementById('fire');
+const waterButton = document.getElementById('water');
+const earthButton = document.getElementById('earth');
+const suggestionsContainer = document.getElementById('suggestions');
 
 function updateText() {
   memeTextElement.textContent = textInputElement.value;
@@ -18,5 +23,27 @@ function loadImage(event) {
   memeTextElement.style.bottom = '5%';
 }
 
+function applyFireBorder() {
+  memeImgContainerElement.style.border = '3px dashed red';
+}
+
+function applyWaterBorder() {
+  memeImgContainerElement.style.border = '5px double blue';
+}
+
+function applyEarthBorder() {
+  memeImgContainerElement.style.border = '6px groove green';
+}
+
+function useSuggestion(event) {
+  imageElement.src = event.target.src;
+  memeTextElement.style.position = 'absolute';
+  memeTextElement.style.bottom = '5%';
+}
+
 textInputElement.addEventListener('keyup', updateText);
 memeInsertElement.addEventListener('change', loadImage);
+fireButton.addEventListener('click', applyFireBorder);
+waterButton.addEventListener('click', applyWaterBorder);
+earthButton.addEventListener('click', applyEarthBorder);
+suggestionsContainer.addEventListener('click', useSuggestion);
