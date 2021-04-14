1be15b4fe050e2e861cc11a957df38beb690f609
Guilherme Pereira de Oliveira <guilhermepdo1508@gmail.com>
2021-04-14 13:02:57 -0300

    Requisito 2

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 const textInput = document.querySelector('#text-input');
 const memeText = document.querySelector('#meme-text');
+const memeInsert = document.querySelector('#meme-insert');
+const memeImage = document.querySelector('#meme-image');
 
 [textInput].forEach((item) => {
   item.addEventListener('keyup', () => {
     memeText.innerText = textInput.value;
+  });
+});
+
+[memeInsert].forEach((item) => {
+  item.addEventListener('change', () => {
+  const uploadImage = memeInsert.files;
+  memeImage.src = URL.createObjectURL(uploadImage[0]);
   });
 });
