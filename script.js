cf8be443dc3e0b1684b4e6cfea7d16a65c542af4
Marco Galindo <marco1408@gmail.com>
2021-04-13 21:25:32 -0300

    requisito 7 finalizado

--- script.js
+++ script.js
@@ -13,8 +13,8 @@ const memeInsert = document.querySelector('#meme-insert');
 const divImage = document.querySelector('#meme-image');
 
 function uploadImage(event) {
+  divImage.innerHTML = '';
   const memeImage = document.createElement('img');
-  memeImage.innerHTML = '';
   memeImage.id = 'image';
   memeImage.src = URL.createObjectURL(event.target.files[0]);
   divImage.appendChild(memeImage);
@@ -24,16 +24,25 @@ memeInsert.addEventListener('input', uploadImage);
 
 const memeImageContainer = document.querySelector('#meme-image-container');
 const fireButton = document.querySelector('#fire');
-fireButton.onclick = function () {
+fireButton.onclick = function redBorder() {
   memeImageContainer.style.border = '3px dashed red';
 };
 
 const waterButton = document.querySelector('#water');
-waterButton.onclick = function () {
+waterButton.onclick = function blueBorder() {
   memeImageContainer.style.border = '5px double blue';
 };
 
 const earthButton = document.querySelector('#earth');
-earthButton.onclick = function () {
+earthButton.onclick = function greenBorder() {
   memeImageContainer.style.border = '6px groove green';
+};
+
+const miniatura = document.querySelector('#miniatura');
+miniatura.onclick = function pegarMiniatura(event) {
+  divImage.innerHTML = '';
+  const memeImage = document.createElement('img');
+  memeImage.id = 'image';
+  memeImage.src = event.target.src;
+  divImage.appendChild(memeImage);
 };
