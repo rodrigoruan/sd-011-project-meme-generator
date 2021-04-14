9f581c5fe5428a5f8728f29d6f0dfc561443fe87
Vitor <vitorguima@gmail.com>
2021-04-14 18:50:06 -0300

    requisitos 1 e 2

--- script2.js
+++ script2.js
@@ -1,8 +1,27 @@
 const inputText = document.querySelector('#text-input');
 const memeContainer = document.querySelector('#meme-image-container');
 const memeText = document.querySelector('#meme-text');
+const memeImageInput = document.querySelector('#meme-insert');
+const imageContainer = document.querySelector('.meme-image');
 
-inputText.addEventListener('keydown', function () {
+// retorna texto inserido em text-input
+inputText.addEventListener('change', function () {
     console.log(inputText.value);
     memeText.innerText = inputText.value;
 });
+
+// retorna imagem inserida
+memeImageInput.addEventListener('change', function() {
+    const file = this.files[0];
+
+    if (file) {
+        const reader = new FileReader();
+        console.log(reader);
+
+        reader.addEventListener('load', function() {
+            imageContainer.setAttribute('src', this.result);
+        })
+
+        reader.readAsDataURL(file);
+    }
+})
