cb246a6ce70f7c93308ecc1b828a8193d0c5dbec
Guilherme Pereira de Oliveira <guilhermepdo1508@gmail.com>
2021-04-14 11:29:44 -0300

    Requisitos 1 ao 5 - Recomeçando

--- script.js
+++ script.js
@@ -1,47 +1,15 @@
 const textInput = document.querySelector('#text-input');
 const memeText = document.querySelector('#meme-text');
-const memeImageContainer = document.querySelector('#meme-image-container');
+const memeInsert = document.querySelector('#meme-insert');
 const memeImage = document.querySelector('#meme-image');
-const memeUploadBtn = document.querySelector('#meme-upload');
-const memeInsertBtn = document.querySelector('#meme-insert');
-const fireBtn = document.querySelector('#fire');
-const waterBtn = document.querySelector('#water');
-const earthBtn = document.querySelector('#earth');
-
-function insertImage() {
-  const uploadedImage = memeUploadBtn.files;
-  memeImage.alt = 'Imagem recebida por upload para fazer memes';
-  memeImage.src = URL.createObjectURL(uploadedImage[0]);
-  memeImageContainer.appendChild(memeImage);
-}
-
-function changeImgBorder(borderStyle) {
-  if (borderStyle === fireBtn) {
-    memeImageContainer.classList.remove('default', 'water', 'earth');
-    memeImageContainer.classList.add('fire');
-  } else if (borderStyle === waterBtn) {
-    memeImageContainer.classList.remove('default', 'fire', 'earth');
-    memeImageContainer.classList.add('water');
-  } else if (borderStyle === earthBtn) {
-    memeImageContainer.classList.remove('default', 'water', 'fire');
-    memeImageContainer.classList.add('earth');
-  }
-}
-
-[memeInsertBtn, fireBtn, waterBtn, earthBtn].forEach((item) => {
-  item.addEventListener('click', (event) => {
-    if (item === memeInsertBtn) {
-      insertImage();
-    } else if (item === fireBtn) {
-      changeImgBorder(fireBtn);
-    } else if (item === waterBtn) {
-      changeImgBorder(waterBtn);
-    } else if (item === earthBtn) {
-      changeImgBorder(earthBtn);
-    }
-  });
-});
+const memeUpload = document.querySelector('#meme-upload');
 
 textInput.addEventListener('keyup', function () {
   memeText.innerText = textInput.value;
+}) 
+});
+
+memeInsert.addEventListener('click', function () {
+  let uploadedImage = memeUpload.files;
+  memeImage.src = URL.createObjectURL(uploadedImage[0]);
 });
