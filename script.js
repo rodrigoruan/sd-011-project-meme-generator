e8a622ad609ecec7e6b5b56ab3f2979f35f87299
Renan <renan.vamo@gmail.com>
2021-04-14 19:30:43 -0300

    Adiciona 3 botões de bordas aos memes

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const textInput = document.getElementById('text-input');
 const memeText = document.getElementById('meme-text');
 const memeImage = document.getElementById('meme-image');
 const uploadImage = document.getElementById('meme-insert');
+const fireButton = document.getElementById('fire');
+const waterButton = document.getElementById('water');
+const earthButton = document.getElementById('earth');
+const memeArea = document.getElementById('meme-image-container');
 
 // Cria um parágrafo de texto e insere dentro da div da imagem
 function createMemeText() {
@@ -18,3 +22,36 @@ function loadFile() {
   });
 }
 loadFile();
+
+function fireBorder() {
+  fireButton.addEventListener('click', () => {
+    if (memeArea.className === 'fire') {
+      memeArea.className = 'border';
+    } else {
+      memeArea.className = 'fire';
+    }
+  });
+}
+fireBorder();
+
+function waterBorder() {
+  waterButton.addEventListener('click', () => {
+    if (memeArea.className === 'water') {
+      memeArea.className = 'border';
+    } else {
+      memeArea.className = 'water';
+    }
+  });
+}
+waterBorder();
+
+function earthBorder() {
+  earthButton.addEventListener('click', () => {
+    if (memeArea.className === 'earth') {
+      memeArea.className = 'border';
+    } else {
+      memeArea.className = 'earth';
+    }
+  });
+}
+earthBorder();
