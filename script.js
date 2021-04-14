d36e38fbf1d2c8366cc8de9a125de1bafb8ae7b9
Renan <renan.vamo@gmail.com>
2021-04-14 11:01:06 -0300

    Altera a fonte do texto e adiciona o texto por cima da imagem

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
-let memeText = document.getElementById('text-input');
-let memeTextDiv = document.getElementById('meme-text');
+const textInput = document.getElementById('text-input');
+const memeText = document.getElementById('meme-text');
+const memeImage = document.getElementById('meme-image');
+const uploadImage = document.getElementById('meme-insert');
 
+// Cria um parágrafo de texto e insere dentro da div da imagem
 function createMemeText() {
-  const newP = document.createElement('p');
-  memeText.addEventListener('keyup', () => {
-    newP.innerText = memeText.value;
-    memeTextDiv.appendChild(newP)
+  textInput.addEventListener('keyup', () => {
+    memeText.innerText = textInput.value;
   });
 }
-createMemeText()
+createMemeText();
+
+// Cria uma imagem dentro da div parent
+function loadFile() {
+  uploadImage.addEventListener('change', (event) => {
+    memeImage.src = URL.createObjectURL(event.target.files[0]);
+  });
+}
+loadFile()
