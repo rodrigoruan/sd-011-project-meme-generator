f4a52c083b9876be6ca06977992137030f5987d1
Thyago Pessoa <flash_thyago@hotmail.com>
2021-04-14 09:24:37 -0300

    Requisito 1

--- script.js
+++ script.js
@@ -1,18 +1,6 @@
 const getInputBox = document.getElementById('text-input');
 const getSpanDiv = document.getElementById('meme-text');
-const getImgContainer = document.getElementById('meme-image-container');
-const getInputButton = document.getElementById('button-input');
 
-function inputText() {
-  getInputButton.addEventListener('click', () => {
-    const createTxt = document.createElement('p');
-    if (getInputBox !== ''){
-    createTxt.innerHTML = getInputBox.value;
-    getImgContainer.appendChild(createTxt);
-    } 
-    if (getInputBox.value){
-      getInputBox.value = '';
-    }
-  });
-}
-inputText()
+getInputBox.addEventListener('input', () => {
+  getSpanDiv.innerText = getInputBox.value;
+});
