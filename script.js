d3a38f4df62dd764525c821346b87c65bc2aba99
Alexandre <naitwa.alexandre@gmail.com>
2021-04-15 12:46:44 -0300

    zeroSeis

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ const para = document.getElementById('meme-text');
 const inputText = document.getElementById('text-input');
 const file = document.getElementById('meme-insert');
 const image = document.getElementById('meme-image');
+const imageBackGr = document.getElementById('meme-image-container');
+const fire = document.getElementById('fire');
+const earth = document.getElementById('earth');
+const water = document.getElementById('water');
 
 // prints paragraph, called inline
 inputText.addEventListener('keyup', (e) => {
@@ -17,4 +21,19 @@ file.addEventListener('change', () => {
     };
     reader.readAsDataURL(file.files[0]);
   }
+});
+
+fire.addEventListener('click', (e) => {
+  const propBorder = window.getComputedStyle(fire, null).getPropertyValue('border');
+  imageBackGr.style.border = propBorder;
+});
+
+earth.addEventListener('click', (e) => {
+  const propBorder = window.getComputedStyle(earth, null).getPropertyValue('border');
+  imageBackGr.style.border = propBorder;
+});
+
+water.addEventListener('click', (e) => {
+  const propBorder = window.getComputedStyle(water, null).getPropertyValue('border');
+  imageBackGr.style.border = propBorder;
 });
