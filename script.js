29b2e27728b321f30b37dca3f5ad6f14477fa720
Alexandre <naitwa.alexandre@gmail.com>
2021-04-15 10:35:49 -0300

    zeroDois

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
+// global variables
 const para = document.getElementById('meme-text');
 const inputText = document.getElementById('text-input');
-console.log(para, inputText);
+const file = document.getElementById('meme-insert');
+const image = document.getElementById('meme-image');
 
 // prints paragraph, called inline
-function getParagraph() {
-  console.log(inputText.value);
-  para.innerHTML = inputText.value;
-}
+inputText.addEventListener('keyup', (e) => {
+  para.innerHTML = e.target.value;
+});
+
+file.addEventListener('change', () => {
+  if (file.files && file.files[0]) {
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      image.setAttribute('src', e.target.result);
+      image.style.width = '500px';
+      image.style.height = '500px';
+    };
+    reader.readAsDataURL(file.files[0]);
+  }
+});
