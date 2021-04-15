677345f22b8e1a4861072ab06f4fb902c5262562
Vitor <vitorguima@gmail.com>
2021-04-15 12:13:26 -0300

    ajustes lint js

--- script2.js
+++ script2.js
@@ -6,59 +6,62 @@ const imageContainer = document.querySelector('#meme-image');
 const fireButton = document.getElementById('fire');
 const waterButton = document.getElementById('water');
 const earthButton = document.getElementById('earth');
-const colossiTemplate = document.getElementById('meme-1')
-const diasTemplate = document.getElementById('meme-2')
-const johnTemplate = document.getElementById('meme-3')
-const rinaldiTemplate = document.getElementById('meme-4')
+const colossiTemplate = document.getElementById('meme-1');
+const diasTemplate = document.getElementById('meme-2');
+const johnTemplate = document.getElementById('meme-3');
+const rinaldiTemplate = document.getElementById('meme-4');
 
 console.log(colossiTemplate);
 
 // retorna texto inserido em text-input
-inputText.addEventListener('keyup', function () {
-	memeText.innerText = inputText.value;
+inputText.addEventListener('keyup', () => {
+  memeText.innerText = inputText.value;
 });
 
+// converte file em url
+function convertImageToUrl() {
+  const file = this.files[0];
+
+  if (file) {
+    const reader = new FileReader();
+
+    reader.addEventListener('load', function () {
+      imageContainer.setAttribute('src', this.result);
+    });
+
+    reader.readAsDataURL(file);
+  }
+}
+
 // retorna imagem inserida
-memeImageInput.addEventListener('change', function () {
-	const file = this.files[0];
-
-	if (file) {
-		const reader = new FileReader();
-
-		reader.addEventListener('load', function () {
-			imageContainer.setAttribute('src', this.result);
-		})
-
-		reader.readAsDataURL(file);
-	}
-})
+memeImageInput.addEventListener('change', convertImageToUrl);
 
 // altera propriedades botões fire water earth
-fireButton.addEventListener('click', function () {
-	memeContainer.style.border = '3px dashed red';
-})
+fireButton.addEventListener('click', () => {
+  memeContainer.style.border = '3px dashed red';
+});
 
-waterButton.addEventListener('click', function () {
-	memeContainer.style.border = '5px double blue';
-})
+waterButton.addEventListener('click', () => {
+  memeContainer.style.border = '5px double blue';
+});
 
-earthButton.addEventListener('click', function () {
-	memeContainer.style.border = '6px groove green';
-})
+earthButton.addEventListener('click', () => {
+  memeContainer.style.border = '6px groove green';
+});
 
 // aplica imagem selecionada ao fundo
-colossiTemplate.addEventListener('click', function () {
-	imageContainer.setAttribute('src', 'imgs/meme1.png');
-})
+colossiTemplate.addEventListener('click', () => {
+  imageContainer.setAttribute('src', 'imgs/meme1.png');
+});
 
-diasTemplate.addEventListener('click', function () {
-	imageContainer.setAttribute('src', 'imgs/meme2.png');
-})
+diasTemplate.addEventListener('click', () => {
+  imageContainer.setAttribute('src', 'imgs/meme2.png');
+});
 
-johnTemplate.addEventListener('click', function () {
-	imageContainer.setAttribute('src', 'imgs/meme3.png');
-})
+johnTemplate.addEventListener('click', () => {
+  imageContainer.setAttribute('src', 'imgs/meme3.png');
+});
 
-rinaldiTemplate.addEventListener('click', function () {
-	imageContainer.setAttribute('src', 'imgs/meme4.png');
-})
+rinaldiTemplate.addEventListener('click', () => {
+  imageContainer.setAttribute('src', 'imgs/meme4.png');
+});
