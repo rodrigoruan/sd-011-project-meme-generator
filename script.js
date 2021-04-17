7bd3548f93c9140b8fdfc1b68f9e8e77e918b71a
Marcelo Leite <marsleite@gmail.com>
2021-04-17 13:29:55 -0300

    ajeitando o codigo do requisito 6.

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const getText = document.getElementById('text-input');
 const printext = document.getElementById('meme-text');
-const btnBorder = document.getElementById('btns');
+const btnFire = document.getElementById('fire');
+const btnWater = document.getElementById('water');
+const btnEarth = document.getElementById('earth');
 const wrapper = document.getElementById('meme-image-container');
 
 function setText() {
@@ -8,26 +10,17 @@ function setText() {
   printext.innerText = item;
 }
 
-function changeBorder(event) {
-  const evento = event.target;
-  if (evento.tagName === 'BUTTON') {
-    switch (evento.id) {
-    case 'fire':
-      wrapper.style.border = '3px dashed red';
-      break;
-    case 'water':
-      wrapper.style.border = '5px double blue';
-      break;
-    case 'earth':
-      wrapper.style.border = '6px groove green';
-      break;
-    default:
-      break;
-    }
-  }
-}
-
 window.onload = () => {
   getText.addEventListener('keyup', setText);
-  btnBorder.addEventListener('click', changeBorder);
+  btnFire.addEventListener('click', () => {
+    wrapper.style.border = '3px dashed rgb(255, 0, 0)';
+  });
+
+  btnWater.addEventListener('click', () => {
+    wrapper.style.border = '5px double rgb(0, 0, 255)';
+  });
+
+  btnEarth.addEventListener('click', () => {
+    wrapper.style.border = '6px groove rgb(0, 128, 0)';
+  });
 };
