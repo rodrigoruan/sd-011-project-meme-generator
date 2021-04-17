fcac9f851d04cf3002331750a46fb5b6ee3057be
Marcelo Leite <marsleite@gmail.com>
2021-04-17 07:56:13 -0300

    testando o requisito 1

---
+++ script.js
@@ -0,0 +1,10 @@
+const memeArea = document.getElementById('meme-image-container');
+const getTextArea = document.getElementById('text-input');
+
+function getText() {
+    let item = document.createElement('h2');
+    item.innerHTML = `${getTextArea.value}`
+    memeArea.appendChild(item)
+}
+
+getText();
