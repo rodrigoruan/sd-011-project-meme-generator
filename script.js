3ddfb3e7b2077b3d51ae176a0309f7324ab43f0b
Marcelo Leite <marsleite@gmail.com>
2021-04-17 12:36:09 -0300

    requisito 6.

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const getText = document.getElementById('text-input');
 const printext = document.getElementById('meme-text');
+const btnFire = document.getElementById('fire');
+const btnWater = document.getElementById('water');
+const btnEarth = document.getElementById('earth');
+const wrapper = document.getElementById('meme-image-container');
 
 function setText() {
   const item = getText.value;
@@ -8,4 +12,15 @@ function setText() {
 
 window.onload = () => {
   getText.addEventListener('keyup', setText);
+  btnFire.addEventListener('click', () => {
+    wrapper.style.border = '3px dashed red';
+  });
+
+  btnWater.addEventListener('click', () => {
+    wrapper.style.border = '5px double blue';
+  });
+
+  btnEarth.addEventListener('click', () => {
+    wrapper.style.border = '6px groove green';
+  });
 };
