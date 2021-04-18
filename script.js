e22dc38dffc176cfd7221b8917af1d155004773b
Mauricio Ruviaro <mauricioruviaro07@gmail.com>
2021-04-18 08:16:35 -0300

    adiciona botoes que alteram borda da imagem quando clicados, adiciona as mesmas classes nos botoes

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
 const textInput = document.querySelector('#text-input');
+const btnFogo = document.querySelector('#fire');
+const btnAgua = document.querySelector('#water');
+const btnTerra = document.querySelector('#earth');
+const memeImageContainer = document.querySelector('#meme-image-container');
+const botaoInsereImagem = document.querySelector('#meme-insert');
 
 textInput.addEventListener('keyup', () => {
   const memeText = document.querySelector('#meme-text');
   memeText.innerHTML = textInput.value;
 });
 
-const botaoInsereImagem = document.querySelector('#meme-insert');
-
 botaoInsereImagem.addEventListener('change', (event) => {
   const srcImg = document.querySelector('#meme-image');
   srcImg.src = URL.createObjectURL(event.target.files[0]);
+});
+
+btnFogo.addEventListener('click', () => {
+  memeImageContainer.className = 'fire';
+});
+
+btnAgua.addEventListener('click', () => {
+  memeImageContainer.className = 'water';
+});
+
+btnTerra.addEventListener('click', () => {
+  memeImageContainer.className = 'earth';
 });
