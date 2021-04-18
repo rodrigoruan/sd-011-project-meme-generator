57f7d2df49a8e2ba3dcdb8f4776e7722e27123fa
renan <renan_menezes07@hotmail.com>
2021-04-18 17:27:43 -0300

    Cumpre o requisito 2.

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-const memeText = document.getElementById('meme-text');
 const textInput = document.getElementById('text-input');
+const memeText = document.getElementById('meme-text');
 
+// Apresenta o texto que foi inserido no input.
 function showText() {
   memeText.innerText = textInput.value;
 }
+showText();
+
+// Faz upload da imagem.
+function showImage() {
+  const imgInput = document.getElementById('meme-insert');
+  const memeImg = document.getElementById('meme-image');
+  const img = document.createElement('img');
+  img.src = imgInput.value;
+  memeImg.appendChild(img);
+  img.setAttribute('width', '250px');
+  img.setAttribute('height', '300px');
+}
+showImage();
