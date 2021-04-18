5c8a638de99d4ee71a0e856ea692cc3c9c3cdb43
Juliana Happatsch <jhappatsch@gmail.com>
2021-04-18 13:40:58 -0300

    Passo 7

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const input = document.getElementById('text-input');
-const container = document.getElementById('meme-image-container')
+const container = document.getElementById('meme-image-container');
 const text = document.getElementById('meme-text');
 const imgInsert = document.getElementById('meme-insert');
 const image = document.getElementById('meme-image');
@@ -15,7 +15,7 @@ imgInsert.addEventListener('change', (event) => {
   image.src = URL.createObjectURL(event.target.files[0]);
 });
 
-fire.addEventListener('click' , () => {
+fire.addEventListener('click', () => {
   container.style.border = 'dashed 3px red';
 });
 
@@ -26,3 +26,26 @@ water.addEventListener('click', () => {
 earth.addEventListener('click', () => {
   container.style.border = 'groove 6px green';
 });
+
+window.onload = () => {
+  const meme1 = document.getElementById('meme-1');
+  const meme2 = document.getElementById('meme-2');
+  const meme3 = document.getElementById('meme-3');
+  const meme4 = document.getElementById('meme-4');  
+
+  meme1.addEventListener('click', () => {
+    image.src = meme1.src;
+  });
+
+  meme2.addEventListener('click', () => {
+    image.src = meme2.src;
+  });
+
+  meme3.addEventListener('click', () => {
+    image.src = meme3.src;
+  });
+
+  meme4.addEventListener('click', () => {
+    image.src = meme4.src;
+  });
+}
