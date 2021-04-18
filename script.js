9e1b5d5afdb917bea47c8ac7c6f3d1f35e51c462
Juliana Happatsch <jhappatsch@gmail.com>
2021-04-18 17:44:23 -0300

    Adiciona CSS

--- script.js
+++ script.js
@@ -1,31 +1,54 @@
-const input = document.getElementById('text-input');
-const container = document.getElementById('meme-image-container');
-const text = document.getElementById('meme-text');
-const imgInsert = document.getElementById('meme-insert');
-const image = document.getElementById('meme-image');
-const fire = document.getElementById('fire');
-const water = document.getElementById('water');
-const earth = document.getElementById('earth');
+const container = document.querySelector('#meme-image-container');
 
-input.onkeyup = () => {
-  text.innerHTML = input.value;
-};
+function writeInput() {
+  const input = document.querySelector('#text-input');
+  const textArea = document.querySelector('#meme-text');
 
-imgInsert.addEventListener('change', (event) => {
-  image.src = URL.createObjectURL(event.target.files[0]);
-});
+  input.onkeyup = () => {
+    textArea.innerText = input.value;
+  }
+}
 
-fire.addEventListener('click', () => {
-  container.style.border = 'dashed 3px red';
-});
+function imageFile() {
+  const imgInsert = document.querySelector('#meme-insert');
+  const image = document.querySelector('#meme-image');
 
-water.addEventListener('click', () => {
-  container.style.border = 'double 5px blue';
-});
+  imgInsert.addEventListener('change', (e) => {
+    image.src = URL.createObjectURL(e.target.files[0]);
+  });
+}
 
-earth.addEventListener('click', () => {
-  container.style.border = 'groove 6px green';
-});
+function fireBorder() {
+  const fire = document.querySelector('#fire');
+
+  fire.addEventListener('click', () => {
+    container.style.border = '3px dashed red';
+  });
+}
+
+function waterBorder() {
+  const water = document.querySelector('#water');
+
+  water.addEventListener('click', () => {
+    container.style.border = '5px double blue';
+  });
+}
+
+function earthBorder() {
+  const earth = document.querySelector('#earth');
+
+  earth.addEventListener('click', () => {
+    container.style.border = '6px groove green';
+  });
+}
+
+function preMeme(meme) {
+  const image = document.querySelector('#meme-image');
+
+  meme.addEventListener('click', () => {
+    image.src = meme.src;
+  });
+}
 
 window.onload = () => {
   const meme1 = document.getElementById('meme-1');
@@ -33,19 +56,13 @@ window.onload = () => {
   const meme3 = document.getElementById('meme-3');
   const meme4 = document.getElementById('meme-4');  
 
-  meme1.addEventListener('click', () => {
-    image.src = meme1.src;
-  });
-
-  meme2.addEventListener('click', () => {
-    image.src = meme2.src;
-  });
-
-  meme3.addEventListener('click', () => {
-    image.src = meme3.src;
-  });
-
-  meme4.addEventListener('click', () => {
-    image.src = meme4.src;
-  });
-}
+  writeInput();
+  imageFile();
+  fireBorder();
+  waterBorder();
+  earthBorder();waterBorder()
+  preMeme(meme1);
+  preMeme(meme2);
+  preMeme(meme3);
+  preMeme(meme4);
+};
