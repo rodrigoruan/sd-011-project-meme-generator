805fb7d7a6471f35e3d83ab2316ae395db8b85c9
Mauricio Ruviaro <mauricioruviaro07@gmail.com>
2021-04-18 07:38:46 -0300

    adiciona imagem  assim que escolhida

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const textInput = document.querySelector('#text-input');
-// const btnEnviar = document.querySelector('#enviar');
 
 textInput.addEventListener('keyup', () => {
   const memeText = document.querySelector('#meme-text');
   memeText.innerHTML = textInput.value;
+});
+
+const botaoInsereImagem = document.querySelector('#meme-insert');
+
+botaoInsereImagem.addEventListener('change', (event) => {
+  const srcImg = document.querySelector('#meme-image');
+  srcImg.src = URL.createObjectURL(event.target.files[0]);
 });
