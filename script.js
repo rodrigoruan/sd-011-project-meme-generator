3c96b1c846784c2e9fb36268c9a4741a7572a14b
Juliana Happatsch <jhappatsch@gmail.com>
2021-04-18 11:57:52 -0300

    Passo 2

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const input = document.getElementById('text-input');
 const text = document.getElementById('meme-text');
+const imgInsert = document.getElementById('meme-insert');
+const image = document.getElementById('meme-image');
 
 input.onkeyup = () => {
-    text.innerHTML = input.value;
-}
+  text.innerHTML = input.value;
+};
+
+imgInsert.addEventListener('change', (event) => {
+  image.src = URL.createObjectURL(event.target.files[0]);
+});
