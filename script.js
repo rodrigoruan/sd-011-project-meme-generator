f9aadad3c06cbba4d43ad3c45f873a3210d8f7c3
vitilevi <vitilevi@hotmail.com>
2021-04-19 21:42:50 -0300

    Feito requisito 6

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const input = document.getElementById('text-input');
 const divText = document.getElementById('meme-text');
 const image = document.getElementById('meme-insert');
+const fireButton = document.getElementById('fire');
+const waterButton = document.getElementById('water');
+const eartButton = document.getElementById('earth');
+const memeContainer = document.getElementById('meme-image-container');
 
 function inputText() {
   const inputValue = input.value;
@@ -15,6 +19,15 @@ function createImage(event) {
 function createEventListeners() {
   input.addEventListener('keyup', inputText);
   image.addEventListener('change', createImage);
+  fireButton.addEventListener('click', () => {
+    memeContainer.style.border = '3px dashed red'
+  })
+  waterButton.addEventListener('click', () => {
+    memeContainer.style.border = '5px double blue'
+  })
+  eartButton.addEventListener('click', () => {
+    memeContainer.style.border = '6px groove rgb(0, 128, 0)'
+  })
 }
 
 function init() {
