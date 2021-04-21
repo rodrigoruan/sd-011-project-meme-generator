14f69340ee4916fcfcbf047ee497c960a34583f0
Vitor HUgo <vitor.h.braz.leite@gmail.com>
2021-04-20 23:07:34 -0300

    Exercicio 3

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-
-
 // Exercício 1 - criar caixa input e mostrar texto input
 let textInput = document.getElementById('text-input');
 textInput.addEventListener('keyup', function(){
@@ -15,4 +13,10 @@ let input = document.getElementById('meme-insert');
 input.addEventListener('input', function(){
     document.getElementById('meme-image').src = URL.createObjectURL(document.getElementById('meme-insert').files[0])
 });
+
+// Exercício 3
+let containerImagem = document.getElementById('meme-image-container');
+containerImagem.style.backgroundColor='white';
+containerImagem.style.border = 'solid black 1px';
+containerImagem.style.display='inline-block';
 
