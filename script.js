8f22406ce622d6a0d4d2109a93bedc42fe60f239
Gabriela Nebbia <gabrielanebbia@gmail.com>
2021-04-24 03:03:51 -0300

    Requisito 3 - moldura no container

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ textInput.addEventListener('keyup', textShow);
 const imageShow = document.getElementById('meme-image');
 
 function memeImage() {
- imageShow.src=URL.createObjectURL(event.target.files[0]);
+  imageShow.src = URL.createObjectURL(event.target.files[0]);
 }
 
 // Referência: https://stackoverflow.com/questions/4459379/preview-an-image-before-it-is-uploaded
