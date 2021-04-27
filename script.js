7742534b12f15e3b73fa1d37242cf0fad973b3ec
Guilherme Pereira de Oliveira <guilhermepdo1508@gmail.com>
2021-04-27 18:25:55 -0300

    Requisito 2 - adicionando div para o p #meme-text

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const textInput = document.querySelector('#text-input');
-const memeText = document.querySelector('#meme-text');
+const memeText = document.querySelector('#meme-text p');
 const memeInsert = document.querySelector('#meme-insert');
 const memeImage = document.querySelector('#meme-image');
+const memeContainer = document.querySelector('#meme-image-container');
 
 [textInput].forEach((item) => {
   item.addEventListener('keyup', () => {
