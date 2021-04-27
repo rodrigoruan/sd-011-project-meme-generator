dcd6cb4d521827bf80111ae214ac2af928ec1296
Guilherme Pereira de Oliveira <guilhermepdo1508@gmail.com>
2021-04-27 19:18:22 -0300

    Refatorando requisitos 1 e 2

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const textInput = document.querySelector('#text-input');
 const memeText = document.querySelector('#meme-text');
 const memeInsert = document.querySelector('#meme-insert');
 const memeImage = document.querySelector('#meme-image');
-const memeContainer = document.querySelector('#meme-image-container');
 
 [textInput].forEach((item) => {
   item.addEventListener('keyup', () => {
@@ -13,6 +12,6 @@ const memeContainer = document.querySelector('#meme-image-container');
 [memeInsert].forEach((item) => {
   item.addEventListener('change', () => {
     const uploadImage = memeInsert.files;
-    memeImage.style.backgroundImage = `url(${URL.createObjectURL(uploadImage[0])})`;
+    memeImage.src = URL.createObjectURL(uploadImage[0]);
   });
 });
