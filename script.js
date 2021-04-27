0cf5c51a07fe181fc8640a347e0e2c9d1398efd3
Guilherme Pereira de Oliveira <guilhermepdo1508@gmail.com>
2021-04-27 19:55:03 -0300

    Requisito 6

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 const textInput = document.querySelector('#text-input');
 const memeText = document.querySelector('#meme-text');
-const memeInsert = document.querySelector('#meme-insert');
-const memeImage = document.querySelector('#meme-image');
 
 [textInput].forEach((item) => {
   item.addEventListener('keyup', () => {
@@ -9,9 +7,29 @@ const memeImage = document.querySelector('#meme-image');
   });
 });
 
+const memeInsert = document.querySelector('#meme-insert');
+const memeImage = document.querySelector('#meme-image');
+
 [memeInsert].forEach((item) => {
   item.addEventListener('change', () => {
     const uploadImage = memeInsert.files;
     memeImage.src = URL.createObjectURL(uploadImage[0]);
+  });
+});
+
+const fireButton = document.querySelector('#fire');
+const waterButton = document.querySelector('#water');
+const earthButton = document.querySelector('#earth');
+const memeImageContainer = document.querySelector('#meme-image-container');
+
+[fireButton, waterButton, earthButton].forEach((item) => {
+  item.addEventListener('click', () => {
+    if (item === fireButton) {
+      memeImageContainer.style.border = '3px dashed red';
+    } else if (item === waterButton) {
+      memeImageContainer.style.border = '5px double blue';
+    } else if (item === earthButton) {
+      memeImageContainer.style.border = '6px groove green';
+    }
   });
 });
