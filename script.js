9e488568b226a27d73c6ba9d5eb1b1a8bf70ff62
¨Hugosomers¨ <hugosomersla@gmail.com>
2021-04-27 11:02:45 -0300

    Requisito 2 completado

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
+// Text e input text
 const text = document.getElementById('meme-text');
 const textInput = document.getElementById('text-input');
 
+// Img e input file
+const img = document.querySelector('#meme-image');
+const imgInput = document.querySelector('#meme-insert');
+
+// Requisito 1
 textInput.addEventListener('keyup', () => {
   text.innerText = textInput.value;
+});
+
+// Requisito 2
+imgInput.addEventListener('change', () => {
+  const file = imgInput.files[0]; // Armazena dentro de "file" o arquivo na posição [0] do imgInput
+  
+  if (file) {
+    const fileReader = new FileReader(); // Lê o arquivo na variavel "file" como um arquivo DATAURL, podemos definir o source
+    
+    fileReader.addEventListener('load', () => {
+      img.src = fileReader.result;
+    });
+    fileReader.readAsDataURL(file);
+  }
+
 });
